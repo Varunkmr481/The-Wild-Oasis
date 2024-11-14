08f820ed3191c2da6938dd20328a582b9103864b
Varunkmr481 <kmrvarun481@gmail.com>
2024-11-14 20:24:43 +0530

    initial files and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 40px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>HLO</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Green Horizons</H1>
+        <Button onClick={() => alert("Checking in...")}>Check In</Button>
+
+        <Input type="number" placeholder="Type the no of guests"></Input>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
