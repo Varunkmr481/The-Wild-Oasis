05b0e8e454e72dcc83199a7aa0e110dcc9082d36
Varunkmr481 <kmrvarun481@gmail.com>
2024-11-16 13:08:03 +0530

    supabase connection with react app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://votfwipbktaagyrzxmei.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdGZ3aXBia3RhYWd5cnp4bWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2NzM3MjYsImV4cCI6MjA0NzI0OTcyNn0.QUaWecx4xz_bHqHDeD4HYv4EeYLVpkR6O-ykQ3dLTmI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
