504ff4899d59f036e45e85edb64519db4e05799c
Varunkmr481 <kmrvarun481@gmail.com>
2024-12-24 19:01:42 +0530

    Prefetching prev & next page bookings data

--- src/features/bookings/feature-hooks/useBookingsData.jsx
+++ src/features/bookings/feature-hooks/useBookingsData.jsx
@@ -1,11 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../../utils/constants";
 
 export function useBookingsData() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
-  // Filter
+  // FILTER
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
@@ -13,12 +15,12 @@ export function useBookingsData() {
       : { field: "status", value: filterValue };
   // { field: "totalPrice", value: 5000, methodName: "gte" };
 
-  // Sorting
+  // SORTING
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  // Pagination
+  // PAGINATION
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const {
@@ -33,6 +35,22 @@ export function useBookingsData() {
 
   const bookings = queryData?.data || [];
   const count = queryData?.count || 0;
+
+  // PRE-FETCHING
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filterValue, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filterValue, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, error, bookings, count };
 }
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -62,8 +63,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
