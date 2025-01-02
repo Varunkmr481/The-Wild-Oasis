c6e9204a020705f24cde77e155050ebd5a069c8d
Varunkmr481 <kmrvarun481@gmail.com>
2025-01-02 06:00:55 +0530

    user signup

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import useSignup from "./feature-hooks/useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isSigningUp } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    // console.log(data);
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
   }
 
   return (
@@ -20,6 +28,7 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
+          disabled={isSigningUp}
           {...register("fullName", {
             required: "This field is required",
           })}
@@ -30,6 +39,7 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isSigningUp}
           {...register("email", {
             required: "This field is required",
             pattern: {
@@ -47,6 +57,7 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isSigningUp}
           {...register("password", {
             required: "This field is required",
             minLength: {
@@ -61,6 +72,7 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isSigningUp}
           {...register("passwordConfirm", {
             required: "This field is required",
             validate: (value) =>
@@ -71,10 +83,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isSigningUp}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isSigningUp}>Create new user</Button>
       </FormRow>
     </Form>
   );
---
+++ src/features/authentication/feature-hooks/useSignup.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../../services/apiAuth";
+import toast from "react-hot-toast";
+
+function useSignup() {
+  const { mutate: signup, isLoading: isSigningUp } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      toast.success(
+        "Account successfully created! Please verify the new account from the user's email address."
+      );
+    },
+    onError: () => {
+      toast.error("Error signing up! Please try again later");
+    },
+  });
+
+  return { signup, isSigningUp };
+}
+
+export default useSignup;
