b89434d4e3ceb6dafd9310e3ee2e1968bb2332e7
Varunkmr481 <kmrvarun481@gmail.com>
2025-01-08 21:53:10 +0530

    display statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import { useRecentBookings } from "./feature-hooks/useRecentBookings";
 import { useRecentStays } from "./feature-hooks/useRecentStays";
+import useCabinsData from "../cabins/feature-hooks/useCabinsData";
 import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useSearchParams } from "react-router-dom";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -11,16 +14,23 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
-  const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const { stays, isLoading: isLoading2, confirmedStays } = useRecentStays();
+  const [searchParams] = useSearchParams();
+  const { bookings, numDays, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabinsData();
 
-  if (isLoading1 || isLoading2) return <Spinner />;
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
   console.log(bookings);
 
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins?.length}
+      ></Stats>
       <div>Today&apos;s activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,51 @@
+import { HiOutlineBriefcase, HiOutlineChartBar } from "react-icons/hi";
+import Stat from "./Stat";
+import { HiOutlineBanknotes, HiOutlineCalendarDays } from "react-icons/hi2";
+import { formatCurrency } from "../../utils/helpers";
+
+function stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  //   1.
+  const numBookings = bookings.length;
+
+  // 2.
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // 3.
+  const checkedIns = confirmedStays.length;
+
+  // 4. occupancy_rate = num of night guest have stayed / total no of nights for each cabin (num nights * num of cabins)
+  const occupancyRate =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      ></Stat>
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      ></Stat>
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkedIns}
+      ></Stat>
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupancyRate * 100) + "%"}
+      ></Stat>
+    </>
+  );
+}
+
+export default stats;
