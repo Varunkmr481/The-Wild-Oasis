3c663803e792cf9a43d8a021f97326db7a9d1291
Varunkmr481 <kmrvarun481@gmail.com>
2025-01-13 10:13:06 +0530

    errorBoundary for render errors

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import Heading from "./Heading";
+import GlobalStyles from "./../styles/GlobalStyles";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
 const StyledErrorFallback = styled.main`
   height: 100vh;
   background-color: var(--color-grey-50);
@@ -29,3 +34,22 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 😊</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={() => resetErrorBoundary()}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
